941797844db8dd01b0a786e9c5c437df2c8cbbaf
Will Sides <32076464+willsides@users.noreply.github.com>
2023-11-10 23:29:58 -0800

    update version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '70d4eaf231f43451d9bf');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e2e48a4ba5b9462714ec');
--- social-icon-link.php
+++ social-icon-link.php
@@ -5,7 +5,7 @@
  * Description:       Another block to add a logo that links to a social media account
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Will Sides
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
