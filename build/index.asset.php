14d987c7b31fde1efbf89b1d54fc04b2010cdb84
Will Sides <32076464+willsides@users.noreply.github.com>
2023-11-10 23:28:43 -0800

    update version info

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c77cebc1d6a76fe45ced');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '70d4eaf231f43451d9bf');
